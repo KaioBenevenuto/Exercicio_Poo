2f42edb15c0b99bb70ed39280b082469e73a7d67
Kaio Benevenuto <kaiohoffmanbeneve@gmail.com>
2022-06-27 13:59:23 -0300

    completo

--- src/exercicio6/Principal.java
+++ src/exercicio6/Principal.java
@@ -1,52 +1,21 @@
 package exercicio6;
 
-import java.util.Scanner;
-
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa();
-		Pessoa p2 = new Pessoa();
-		Pessoa p3 = new Pessoa();
-		Scanner leia = new Scanner(System.in);
+		Pessoa p[] = new Pessoa[3];
 		
-		System.out.print("Digite seu nome: ");
-		p1.nome = leia.next();
-		System.out.print("Digite sua idade: ");
-		p1.idade = leia.nextInt();
-		System.out.print("Digite seu sexo: ");
-		p1.sexo = leia.next();
-		System.out.println("-------------------------------");
-		System.out.print("Digite seu nome: ");
-		p2.nome = leia.next();
-		System.out.print("Digite sua idade: ");
-		p2.idade = leia.nextInt();
-		System.out.print("Digite seu sexo: ");
-		p2.sexo = leia.next();
-		System.out.println("-------------------------------");
-		System.out.print("Digite seu nome: ");
-		p3.nome = leia.next();
-		System.out.print("Digite sua idade: ");
-		p3.idade = leia.nextInt();
-		System.out.print("Digite seu sexo: ");
-		p3.sexo = leia.next();
+		p[0] = new Pessoa("Cleitin", "M", 18);
+		p[1] = new Pessoa("Jubileu", "M", 40);
+		p[2] = new Pessoa("Fernanda", "F", 25);
 		
-		System.out.println("===============================");
-		System.out.println("1-Pessoa");
-		System.out.println("Nome: " + p1.nome);
-		System.out.println("Idade: " + p1.idade);
-		System.out.println("Sexo: " + p1.sexo);
-		System.out.println("-------------------------------");
-		System.out.println("2-Pessoa");
-		System.out.println("Nome: " + p2.nome);
-		System.out.println("Idade: " + p2.idade);
-		System.out.println("Sexo: " + p2.sexo);
-		System.out.println("-------------------------------");
-		System.out.println("3-Pessoa");
-		System.out.println("Nome: " + p3.nome);
-		System.out.println("Idade: " + p3.idade);
-		System.out.println("Sexo: " + p3.sexo);
+		for(int cont = 0; cont < p.length; cont++) {
+			System.out.println("------------------------------");
+			System.out.println("Nome: " + p[cont].nome);
+			System.out.println("Sexo: " + p[cont].sexo);
+			System.out.println("Idade: " + p[cont].idade);
+		}
 	}
 
 }
