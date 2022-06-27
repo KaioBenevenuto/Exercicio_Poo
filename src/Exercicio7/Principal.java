bfc1b58f38284c309691e5fc5e1041cf65d06594
Kaio Benevenuto <kaiohoffmanbeneve@gmail.com>
2022-06-27 12:00:55 -0300

    completo

---
+++ src/Exercicio7/Principal.java
@@ -0,0 +1,26 @@
+package Exercicio7;
+
+public class Principal extends Elevador {
+
+
+	public static void main(String[] args) {
+		
+		Elevador e = new Elevador(5, 3);
+		e.entrar();
+		e.entrar();
+		e.entrar();
+		e.descer();
+		
+		System.out.println("------------------------------------------");
+		System.out.println("Total de andar: " + e.getTotAndar());
+		if(e.getAndarAtual() == 0) {
+			System.out.println("Andar atual: Terreo");
+		}else {
+			System.out.println("Andar atual: " + e.getAndarAtual());
+		}
+		System.out.println("Capacidade do elevador: "+ e.getCapacidadeElevador() + " Pessoas");
+		System.out.println("Quantidade de pessoas no elevador: " + e.getPessoasPresentes());
+		System.out.println("------------------------------------------");
+	}
+
+}
